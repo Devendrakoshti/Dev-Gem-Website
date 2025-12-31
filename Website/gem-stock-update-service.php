e034763b5571736ed3210ad9c3e7481afdaf59dc
toswebdevuser-03 <tos.webdevuser03@gmail.com>
2025-12-31 17:04:13 +0530

    changes

--- Website/gem-stock-update-service.php
+++ Website/gem-stock-update-service.php
@@ -327,9 +327,9 @@ Success Note: The system will give a success prompt, and the internal 90-day cou
           <div class="row g-5 align-items-center">
             <div class="col-lg-8">
               <div class="h-100 bg-white">
-                <h2 class="h2-title mb-3">Keep Your GeM Business Running 24/7! Start Your Stress-Free Compliance  <b> Partnership Today!</b>
+                <h2 class="h2-title mb-3">Keep Your GeM Business  <b> Running 24/7!</b>
                 </h2>
-                <p class="fs-18 fw-medium mb-4">The next tender could be yours! Stop letting the registration paperwork slow you down. Let our friendly and expert team handle the process so you can focus on running your business.</p>
+                <p class="fs-18 fw-medium mb-4">Start Your Stress-Free Compliance Partnership Today!</p>
                 <a title="Explore More" class="btn-primary" href="/contact-us.php"> Start Your GeM Registration Today! <span>
                     <svg id="Layer_1" enablebackground="new 0 0 100 100" height="40" viewBox="0 0 100 100" fill="#f70629" width="40" xmlns="http://www.w3.org/2000/svg">
                       <path d="m50 10.75c-18.266 0-34.562 13.129-38.383 31.007-1.909 8.933-.623 18.432 3.636 26.515 4.099 7.779 10.819 14.066 18.859 17.629 8.363 3.707 17.964 4.353 26.754 1.825 8.48-2.438 15.999-7.789 21.118-14.972 10.703-15.017 9.272-36.111-3.32-49.567-7.38-7.886-17.862-12.437-28.664-12.437zm18.829 41.347-10.7 10.958c-2.709 2.775-6.991-1.429-4.293-4.191l5.399-5.529h-25.586c-1.817 0-3.333-1.517-3.333-3.333s1.517-3.333 3.333-3.333h25.458l-5.506-5.505c-2.736-2.736 1.506-6.979 4.242-4.243l10.961 10.96c1.162 1.161 1.173 3.041.025 4.216z"></path>
