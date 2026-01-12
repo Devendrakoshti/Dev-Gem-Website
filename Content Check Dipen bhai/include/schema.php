8519e8c6e6a8432207d73dd5b47919504fb5b8ca
toswebdevuser-02 <architecturalbim9@gmail.com>
2026-01-12 20:08:39 +0530

    Add Code

---
+++ Content Check Dipen bhai/include/schema.php
@@ -0,0 +1,20 @@
+<?php if($activePage == 'index') { ?>
+
+<!-- Home Page -->
+
+	<!--Schema Code Here Home page-->
+
+	<!-- Home Page END-->
+
+<?php } ?>
+
+<?php if($activePage == 'about-us') { ?>
+
+	<!-- About us-->
+
+	<!--Schema Code Here About Us page-->
+
+	<!-- About us END-->
+
+<?php } ?>
+
