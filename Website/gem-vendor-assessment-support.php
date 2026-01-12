9b0864aa2c318b00c2a31d543539bf3dda1ac6e0
toswebdevuser-03 <tos.webdevuser03@gmail.com>
2026-01-12 20:13:02 +0530

    changes

--- Website/gem-vendor-assessment-support.php
+++ Website/gem-vendor-assessment-support.php
@@ -38,11 +38,11 @@
     <!-- header start --> <?php include 'includes/header.php';?>
     <!-- header end -->
     <!-- banner start  -->
-    <div class="service-banner gem-gujarat-vendor-assessment-support-banner">
+         <div class="service-banner">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-10">
-            <div class="text-white">
+        <div class="row flex-wrap-reverse align-items-center">
+          <div class="col-lg-6">
+            <div class="h-100">
               <div class="page-path">
                 <span>
                   <i class="fa-solid fa-house text-red me-1"></i> Home <i class="fa-solid fa-chevron-right mx-1 fs-12"></i> GeM Vendor Assessment Support </span>
@@ -57,9 +57,15 @@
               </a>
             </div>
           </div>
+          <div class="col-lg-6">
+            <div class="h-100">
+              <div class="hero-banner-image">
+                <img src="images/gem-order-acceptance-guide-banner.webp" alt="Gem Order Acceptance Guide Banner Image" title="Gem Order Acceptance Guide Banner Image" class="img-fluid" width="1000" height="1000">
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      
     </div>
     <!-- banner end  -->
     <!-- Why GeM Problem Section Start -->
